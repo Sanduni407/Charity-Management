fc3056e12f42555b0b98b38a29565097ae66a4ce
Sanduni407 <sandunidias05@gmail.com>
2025-08-20 16:52:30 +0530

    modified app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,9 @@ import HelpRequestForm from './pages/HelpRequestForm'
 import LoginPage from './pages/LoginPage'
 import BeneficiaryProfile from './pages/BeneficiaryProfile'
 import AdminDashboard from './pages/AdminDashboard '
+import AdminHelpRequests from './pages/AdminHelpRequests'
+import CreatePostPage from './pages/CreatePostPage'
+import DonateRequests from './pages/donateRequests'
 
 const App = () => {
   return (
@@ -19,6 +22,9 @@ const App = () => {
              <Route path='/login' element={<LoginPage/>} />
               <Route path='/beneficiary-profile' element={<BeneficiaryProfile/>} />
                <Route path='/admin-dashboard' element={<AdminDashboard/>} />
+                <Route path='/admin-view-requests' element={<AdminHelpRequests/>} />
+                 <Route path='/admin/create-post/:requestId' element={<CreatePostPage/>} />
+                  <Route path='/donate' element={<DonateRequests/>} />
       </Routes>
     </div>
   )
